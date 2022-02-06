b12af8cbf0bbc5fac21f555274d6f8e70dfd8afc
tatigonch <tanya.avror@gmail.com>
2022-02-06 22:22:20 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stackArray = [];
+
+    for( let i = 0; i < str.length; i++ ){
+        for ( let brack of bracketsConfig ) {
+            if( str[i] == brack[0]  && brack[0] == brack[1]
+                && brack[0] == stackArray[stackArray.length - 1] ){
+                stackArray.pop();
+                break;
+            }
+            if ( str[i] == brack[0] ){
+                stackArray.push( str[i] );
+                break;
+            }
+            if( str[i] == brack[1] ){
+                if( brack[0] != stackArray.pop() ){
+                    return false;
+                }
+            }
+        }
+    }
+    if( stackArray.length ) return false;
+    return true;
 }
